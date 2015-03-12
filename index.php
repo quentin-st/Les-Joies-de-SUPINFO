23ff0b6277ebcb3033d233291bea7aa7aa89c1af
Quentin S <chteuchteu@gmail.com>
2015-03-12 21:07:33 +0100

    Add HTML, CSS and JS base

---
+++ index.php
@@ -0,0 +1,27 @@
+<?php
+require_once('ljs-includes.php');
+
+/*
+ * This is the main page of the project
+ */
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <title>Les Joies de Supinfo</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="">
+    <meta name="author" content="Quentin Stoeckel, Frédéric Strebler, Thomas Nold">
+    <link href="inc/css/bootstrap.min.css" rel="stylesheet" />
+    <link href="inc/css/bootstrap-responsive.min.css" rel="stylesheet" />
+    <link href="inc/css/style.css" rel="stylesheet" />
+</head>
+<body>
+    <div class="container">
+    </div>
+
+    <script src="inc/js/bootstrap.min.js" ></script>
+    <script src="inc/js/jquery-1.11.2.min.js" ></script>
+</body>
+</html>
