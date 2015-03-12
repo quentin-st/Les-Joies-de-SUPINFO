37fb1880ff21a0de6d70d67009fabfcd6d2f689e
Quentin S <chteuchteu@gmail.com>
2015-03-12 21:48:01 +0100

    Add some design ; add submittedBy field to Gif table

--- index.php
+++ index.php
@@ -16,9 +16,39 @@ require_once('ljs-includes.php');
     <link href="inc/css/bootstrap.min.css" rel="stylesheet" />
     <link href="inc/css/bootstrap-responsive.min.css" rel="stylesheet" />
     <link href="inc/css/style.css" rel="stylesheet" />
+    <link href='http://fonts.googleapis.com/css?family=Roboto:500,300' rel='stylesheet' type='text/css'>
 </head>
 <body>
     <div class="container">
+        <div class="header">
+            <h1>Les Joies de Supinfo</h1>
+        </div>
+        <div class="content">
+            <div class="gifItem">
+                <img src="http://ljdchost.com/cbel57q.gif" alt="Quand je vois les specs du nouveau projet" />
+                <div class="catchPhrase">Quand je vois les specs du nouveau projet</div>
+                <div class="gifItemFooter">
+                    <div>Posté le <span>01/02/2015</span></div>
+                    <div>Proposé par <span>louim</span></div>
+                </div>
+            </div>
+            <div class="gifItem">
+                <img src="http://ljdchost.com/CuNH79E.gif" alt="Quand le chef cherche quelqu’un pour taffer sur un vieux projet avec lui" />
+                <div class="catchPhrase">Quand le chef cherche quelqu’un pour taffer sur un vieux projet avec lui</div>
+                <div class="gifItemFooter">
+                    <div class="submittedBy">louim</div>
+                    <div class="submissionDate">01/02/2015</div>
+                </div>
+            </div>
+            <div class="gifItem">
+                <img src="http://ljdchost.com/sk2J56x.gif" alt="Quand je déplace mon projet et que j’ai oublié de copier ses fichiers de référence" />
+                <div class="catchPhrase">Quand je déplace mon projet et que j’ai oublié de copier ses fichiers de référence</div>
+                <div class="gifItemFooter">
+                    <div class="submittedBy">louim</div>
+                    <div class="submissionDate">01/02/2015</div>
+                </div>
+            </div>
+        </div>
     </div>
 
     <script src="inc/js/bootstrap.min.js" ></script>
