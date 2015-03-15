09c0ba57d27c2c28d123b0802eec73e6fff2f16f
Quentin S <chteuchteu@gmail.com>
2015-03-15 20:11:00 +0100

    Migrate to bootstrap last version

--- ljs-template/header.part.php
+++ ljs-template/header.part.php
@@ -15,12 +15,13 @@ $isHomePage = isset($homePage) && $homePage;
 <html lang="en">
 <head>
     <meta charset="utf-8">
-    <title><?= $pageName ?>Les Joies de Supinfo</title>
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title><?= $pageName ?>Les Joies de Supinfo</title>
     <meta name="description" content="">
     <meta name="author" content="Quentin Stoeckel, Frédéric Strebler, Thomas Nold">
     <link href="<?= WEBSITE_URL ?>inc/css/bootstrap.min.css" rel="stylesheet" />
-    <link href="<?= WEBSITE_URL ?>inc/css/bootstrap-responsive.min.css" rel="stylesheet" />
+    <link href="<?= WEBSITE_URL ?>inc/css/bootstrap-theme.min.css" rel="stylesheet" />
     <link href="<?= WEBSITE_URL ?>inc/css/style.css" rel="stylesheet" />
     <link href='http://fonts.googleapis.com/css?family=Roboto:500,300' rel='stylesheet' type='text/css'>
     <script src="<?= WEBSITE_URL ?>inc/js/jquery-1.11.2.min.js"></script>
@@ -29,11 +30,11 @@ $isHomePage = isset($homePage) && $homePage;
 <div class="container">
     <div class="header <? if($isHomePage) echo 'homeHeader'; ?>">
         <div class="row">
-            <div class="span4">
+            <div class="col-md-6">
                 <a href="<?= WEBSITE_URL ?>"><h1>Les Joies de Supinfo</h1></a>
             </div>
-            <div class="span4"></div>
-            <div class="span4">
+            <div class="col-md-2"></div>
+            <div class="col-md-4">
                 <ul class="menu">
                     <li><a href="<?= WEBSITE_URL ?>submit.php">Proposer un gif</a></li>
                 </ul>
