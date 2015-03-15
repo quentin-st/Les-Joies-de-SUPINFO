bda7a320376036ea91e604fe6e9d11440c35c956
Quentin S <chteuchteu@gmail.com>
2015-03-15 17:16:06 +0100

    Add footer

--- ljs-template/footer.part.php
+++ ljs-template/footer.part.php
@@ -1,4 +1,7 @@
     </div>
+    <div class="footer">
+        <a href="https://github.com/chteuchteu/Les-Joies-de-Supinfo">Contribuer</a> &bull; <a href="<?php echo WEBSITE_URL ?>rulesOfTheGame.php">Conditions d'utilisation</a>
+    </div>
     <script src="<?php echo WEBSITE_URL ?>inc/js/bootstrap.min.js"></script>
 </body>
 </html>
