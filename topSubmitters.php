3416bd9f06b164a5133a3fae2fb8d706a5a3330e
Quentin S <chteuchteu@gmail.com>
2015-03-19 13:44:13 +0100

    Top submitters enhancements

--- topSubmitters.php
+++ topSubmitters.php
@@ -4,12 +4,12 @@ require_once('ljs-includes.php');
 include(ROOT_DIR.'/ljs-template/header.part.php');
 
 ?>
-<div class="content">
+<div class="content topSubmitters">
     <h1>Top contributeurs</h1>
     <ol>
-        <? foreach (getTopContributors() as $topContributor) ?>
-        <li><?= $topContributor['contributor'] ?> (<?= $topContributor['gifsCount'] ?> gif(s))</li>
+        <? foreach (getTopContributors() as $topCont) { ?>
+        <li><?= $topCont['contributor'] ?> <span> - <?= $topCont['gifsCount'] ?> gif<?= intval($topCont['gifsCount']) > 1 ? 's' : '' ?></span></li>
+        <? } ?>
     </ol>
-
 </div>
 <? include(ROOT_DIR.'/ljs-template/footer.part.php'); ?>
