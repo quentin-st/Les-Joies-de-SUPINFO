b40e3bd9c0fc062a8de24c3f2ae5c5f8221d632c
Thomson2306 <thomas.nold@gmail.com>
2015-03-23 22:54:39 +0100

    Some formatting in script.js

--- inc/js/script.js
+++ inc/js/script.js
@@ -1,29 +1,26 @@
 $(document).ready(function() {
+
     // Avoid the hash (#) in the URL
     $('a[href="#"]').on('click', function(e){ e.preventDefault() });
-    
-    function click() {
-        $('.report').click(function(){
-            // Ajax call to abuse.php
 
+    function report() {
+        $('.report').click(function(){
+
+            // Ajax call to abuse.php
             $.ajax({
                 url : 'abuse.php',
                 type : 'POST',
                 data : 'id=' + $(this).attr('id'),
                 context: this,
-                success : function(msg){
+                success : function(){
                     $(this).parent().append('<div class="alert alert-danger">Ce gif a bien été signalé, nous allons le vérifier d\'un peu plus près.</div>');
                 }
             });
 
             // Disable all "Report" link for 10 seconds to avoid spam
             $('.report').unbind();
-
-            setTimeout(function(){
-                click();
-            }, 10000);
-
+            setTimeout(function(){ report(); }, 10000);
         });
     }
-    click();
+    report();
 });
