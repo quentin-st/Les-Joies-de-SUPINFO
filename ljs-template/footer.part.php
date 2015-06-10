b262a2c8821f2ccefca13dc5e03b8085dadae3b0
Quentin S <chteuchteu@gmail.com>
2015-06-10 12:00:04 +0200

    Create base symfony application

--- ljs-template/footer.part.php
+++
@@ -1,7 +0,0 @@
-    </div>
-    <div class="footer">
-        <a href="https://github.com/chteuchteu/Les-Joies-de-Supinfo">Contribuer</a> &bull; <a href="<?= WEBSITE_URL ?>rulesOfTheGame.php">Conditions d'utilisation</a>
-    </div>
-    <script src="<?= WEBSITE_URL ?>inc/js/bootstrap.min.js"></script>
-</body>
-</html>
--- uploads/readme.txt
+++
@@ -1,5 +0,0 @@
-/*
- * This directory contains all uploaded & downloaded GIFs
- *	(it must be writable by web server user (www-data on Linux),
- *		an exception will be thrown if not)
- */
