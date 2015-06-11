776fd7994f2cbcbc5003264cae737ea4415c4272
Quentin S <chteuchteu@gmail.com>
2015-06-11 10:59:23 +0200

    Fix report JS

--- src/LjdsBundle/Resources/public/js/script.js
+++ src/LjdsBundle/Resources/public/js/script.js
@@ -3,25 +3,26 @@ $(document).ready(function() {
     // Avoid the hash (#) in the URL
     $('a[href="#"]').on('click', function(e){ e.preventDefault() });
 
-    function report() {
-        $('.report').click(function(){
-
-            // Ajax call to abuse.php
-            $.ajax({
-                url : '/abuse',
-                type : 'POST',
-                data : 'id=' + $(this).attr('data-id'),
-                context: this,
-                success : function(data){
-                    $(this).parent().append('<div class="alert ' + data.class + ' reported">' + data.message + '</div>');
-                    setTimeout(function(){ $('.reported').remove(); }, 10000);
-                }
-            });
-
-            // Disable all "Report" link for 10 seconds to avoid spam
-            $('.report').unbind();
-            setTimeout(function(){ report(); }, 10000);
-        });
-    }
     report();
 });
+
+function report() {
+	$('.report').click(function(){
+
+		// Ajax call to abuse.php
+		$.ajax({
+			url : '/abuse',
+			type : 'POST',
+			data : 'id=' + $(this).attr('data-id'),
+			context: this,
+			success : function(data){
+				$(this).parent().append('<div class="alert ' + data.class + ' reported">' + data.message + '</div>');
+				setTimeout(function(){ $('.reported').remove(); }, 10000);
+			}
+		});
+
+		// Disable all "Report" link for 10 seconds to avoid spam
+		$('.report').unbind();
+		setTimeout(function(){ report(); }, 10000);
+	});
+}
