773bf35bb8f94201d42e085b95621f6f31b91f6d
Quentin Stoeckel <chteuchteu@gmail.com>
2015-12-01 20:17:10 +0100

    getFacebookLikes(): use API JSON format

--- src/LjdsBundle/Helper/FacebookHelper.php
+++ src/LjdsBundle/Helper/FacebookHelper.php
@@ -3,31 +3,34 @@
 namespace LjdsBundle\Helper;
 
 use LjdsBundle\Entity\Gif;
-use SimpleXMLElement;
 use Symfony\Component\Routing\Router;
 
 class FacebookHelper
 {
-	public static function getFacebookLikes(/** @var Gif[] $gifs */$gifs, Router $router)
+	/**
+	 * Get a list of published Gifs sorted by Facebook likes (& shares & comments)
+	 * @param $gifs Gif[]
+	 * @param Router $router
+	 * @return Gif[]
+	 */
+	public static function getFacebookLikes(array $gifs, Router $router)
 	{
-        // TODO this should be enhanced
-
 		// Build api call url
-		$urls = '';
+		$urls = [];
 		foreach ($gifs as $gif)
-			$urls .= $router->generate('gif', ['permalink' => $gif->getPermalink()], true).',';
+			$urls[] = urlencode($router->generate('gif', ['permalink' => $gif->getPermalink()], true));
 
-		$apiUrl = 'http://api.facebook.com/restserver.php?method=links.getStats&urls=' . $urls;
+		$apiUrl = 'http://api.facebook.com/restserver.php?method=links.getStats&urls=' . implode(',', $urls) . '&format=json';
 		$result = file_get_contents($apiUrl);
-		$xmlRes = new SimpleXMLElement($result);
+		$json = json_decode($result, true);
 
 		$likes = [];
-		for ($i=0; $i<count($xmlRes[0]); $i++) {
-			$url = $xmlRes->link_stat[$i]->url;
-			$likesCount = intval($xmlRes->link_stat[$i]->total_count);
+		foreach ($json as $item) {
+			$url = $item['url'];
+			$likesCount = intval($item['total_count']);
 
-            // Each time a gif is published, it is posted on the Facebook page's wall
-            // Se it has been liked if the likesCount is > than 1
+            // Each time a gif is published, it is posted on the Facebook page's wall:
+            // It has been liked if the likesCount is > 1
             if ($likesCount > 1) {
                 $likes[] = [
                     'url' => $url,
