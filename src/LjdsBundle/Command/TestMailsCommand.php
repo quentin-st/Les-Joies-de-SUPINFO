2ad82706a243193eef2ff0596648fc33bc5530f7
Quentin Stoeckel <chteuchteu@gmail.com>
2015-12-22 20:38:16 +0100

    Create MailService alongside with its sendGifApprovedMail and sendGifPublishedMail methods

---
+++ src/LjdsBundle/Command/TestMailsCommand.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace LjdsBundle\Command;
+
+use Symfony\Bundle\FrameworkBundle\Command\ContainerAwareCommand;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+
+class TestMailsCommand extends ContainerAwareCommand
+{
+	protected function configure()
+	{
+		$this
+			->setName('ljds:test-mails')
+			->setDescription('Tests mails')
+			->addArgument('to');
+	}
+
+	protected function execute(InputInterface $input, OutputInterface $output)
+	{
+		$to = $input->getArgument('to');
+
+		if (!$to) {
+			$dialog = $this->getHelper('dialog');
+			$to = $dialog->ask($output, 'Please enter "to" address: ');
+		}
+
+		$this->getContainer()->get('app.mail_service')->sendTestMail($to);
+		$output->writeln('Mail sent.');
+	}
+}
