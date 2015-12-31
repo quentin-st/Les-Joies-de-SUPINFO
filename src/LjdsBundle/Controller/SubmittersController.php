a03b7295913e5f9ea8231e47884fd2212d2a461e
Quentin Stoeckel <chteuchteu@gmail.com>
2015-12-31 15:46:13 +0100

    Enhance top-submitters page styling

--- src/LjdsBundle/Controller/SubmittersController.php
+++ src/LjdsBundle/Controller/SubmittersController.php
@@ -23,7 +23,64 @@ class SubmittersController extends Controller
         $gifs = $gifRepo->findByGifState(GifState::PUBLISHED);
 
         $params = [
-            'submitters' => FacebookHelper::getFacebookLikesGroupedBySubmitter($gifs, $this->get('router'))
+            //'submitters' => FacebookHelper::getFacebookLikesGroupedBySubmitter($gifs, $this->get('router')),
+            'submitters' => [
+                [
+                    'submitter' => 'Frédéric',
+                    'likes' => '34',
+                    'gifs_count' => '68'
+                ],
+                [
+                    'submitter' => 'chteuchteu',
+                    'likes' => '19',
+                    'gifs_count' => '30'
+                ],
+                [
+                    'submitter' => 'Alex',
+                    'likes' => '92',
+                    'gifs_count' => '31'
+                ],
+                [
+                    'submitter' => 'Lucas',
+                    'likes' => '62',
+                    'gifs_count' => '17'
+                ],
+                [
+                    'submitter' => 'Koala',
+                    'likes' => '43',
+                    'gifs_count' => '10'
+                ],
+                [
+                    'submitter' => 'vins',
+                    'likes' => '38',
+                    'gifs_count' => '2'
+                ],
+                [
+                    'submitter' => 'Krhyt',
+                    'likes' => '36',
+                    'gifs_count' => '4'
+                ],
+                [
+                    'submitter' => 'Napo',
+                    'likes' => '30',
+                    'gifs_count' => '6'
+                ],
+                [
+                    'submitter' => 'Shabbollin',
+                    'likes' => '23',
+                    'gifs_count' => '5'
+                ],
+                [
+                    'submitter' => 'Hop5',
+                    'likes' => '22',
+                    'gifs_count' => '3'
+                ],
+                [
+                    'submitter' => 'David',
+                    'likes' => '21',
+                    'gifs_count' => '4'
+                ],
+            ]
         ];
 
         return $this->render('LjdsBundle:Submitters:top.html.twig', $params);
