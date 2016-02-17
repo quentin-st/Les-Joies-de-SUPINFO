52d7753ff37535e6369795646a60eae55473c81e
Quentin Stoeckel <chteuchteu@gmail.com>
2016-02-17 10:18:08 +0100

    Add ljds:debug-likes-all command

---
+++ src/LjdsBundle/Command/DebugLikesCountAllCommand.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace LjdsBundle\Command;
+
+use LjdsBundle\Entity\Gif;
+use LjdsBundle\Entity\GifRepository;
+use LjdsBundle\Entity\GifState;
+use LjdsBundle\Service\FacebookLikesService;
+use Symfony\Bundle\FrameworkBundle\Command\ContainerAwareCommand;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+
+class DebugLikesCountAllCommand extends ContainerAwareCommand
+{
+    protected function configure()
+    {
+        $this
+            ->setName('ljds:debug-likes-all');
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        $em = $this->getContainer()->get('doctrine.orm.entity_manager');
+        /** @var GifRepository $gifRepo */
+        $gifRepo = $em->getRepository('LjdsBundle:Gif');
+        $gifs = $gifRepo->findByGifState(GifState::PUBLISHED);
+
+        // Get likes!
+        /** @var FacebookLikesService $facebookLikesService */
+        $facebookLikesService = $this->getContainer()->get('app.facebook_likes');
+        $facebookLikesService->fetchLikes($gifs);
+
+        $likes = [];
+        /** @var Gif $gif */
+        foreach ($gifs as $gif)
+        {
+            $likes[$gif->getPermalink()] = $gif->getLikes();
+        }
+
+        $output->writeln(json_encode($likes));
+    }
+}
